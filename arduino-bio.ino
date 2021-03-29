658af4674d2d9c4071f92afff454295ad07e161a
aadhi0610 <47136906+aadhi0610@users.noreply.github.com>
2021-03-29 15:06:04 +0530

    Add files via upload
    Thingspeak code

---
+++ arduino-bio.ino
@@ -0,0 +1,85 @@
+// Robo India Tutorial 
+// Simple code upload the tempeature and humidity data using thingspeak.com
+// Hardware: NodeMCU,DHT11
+
+#
+
+#include <ESP8266WiFi.h>
+ 
+String apiKey = "UCL4FYA4BNO60SSA";     //  Enter your Write API key from ThingSpeak
+
+const char *ssid =  "Rose";     // replace with your wifi ssid and wpa2 key
+const char *pass =  "rose@1234";
+const char* server = "api.thingspeak.com";
+
+        //pin where the dht11 is connected
+ 
+
+
+WiFiClient client;
+ 
+void setup() 
+{
+       Serial.begin(115200);
+       delay(10);
+       
+ 
+       Serial.println("Connecting to ");
+       Serial.println(ssid);
+ 
+ 
+       WiFi.begin(ssid, pass);
+ 
+      while (WiFi.status() != WL_CONNECTED) 
+     {
+            delay(500);
+            Serial.print(".");
+     }
+      Serial.println("");
+      Serial.println("WiFi connected");
+ 
+}
+ 
+void loop() 
+{
+  
+     
+      unsigned int t = analogRead(A0);
+      
+              if ( isnan(t)) 
+                 {
+                     Serial.println("Failed to read from DHT sensor!");
+                      return;
+                 }
+
+                         if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+                      {  
+                            
+                             String postStr = apiKey;
+                             postStr +="&field1=";
+                             postStr += String(t);
+                             
+                             postStr += "\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");
+                             client.print(postStr);
+ 
+                             Serial.print("Temperature: ");
+                             Serial.print(t);
+                             Serial.print(" degrees Celcius ");
+                             
+                        }
+          client.stop();
+ 
+          Serial.println("Waiting...");
+  
+  // thingspeak needs minimum 15 sec delay between updates, i've set it to 30 seconds
+  delay(10000);
+}
